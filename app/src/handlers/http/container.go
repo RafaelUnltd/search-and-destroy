867f0b8e9994824b4186deafb503f628e4a57f58
RafaelUnltd <rafael.willian.personal@gmail.com>
2022-12-09 22:47:20 -0300

    Algumas tentativas

--- app/src/handlers/http/container.go
+++ app/src/handlers/http/container.go
@@ -5,6 +5,7 @@ import (
 	"poly-shooters/app/src/interfaces"
 	"poly-shooters/app/src/services"
 
+	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
 )
 
@@ -19,8 +20,10 @@ func NewHandlersContainer(s services.ServicesContainer) HandlersContainer {
 }
 
 func (h HandlersContainer) RegisterRoutes(httpServer *echo.Echo) {
+	httpServer.POST("/login", h.PlayersHandler.AuthenticatePlayer)
+
 	players := httpServer.Group("/players")
+	players.Use(middleware.JWT([]byte("TODO")))
 	players.GET("", h.PlayersHandler.ListPlayers)
 	players.POST("", h.PlayersHandler.CreatePlayer)
-	players.POST("/login", h.PlayersHandler.AuthenticatePlayer)
 }
